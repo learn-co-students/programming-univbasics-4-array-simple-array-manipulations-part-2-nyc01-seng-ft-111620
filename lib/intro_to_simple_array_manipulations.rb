b82065bbc49892e5a22f5f00471ef34b95a6fcab
Jakob Persons <jakobep24@gmail.com>
2020-10-19 17:01:49 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+# num1 = [1, 2, 3, 4]
+# num2 = [5, 6, 7, 8]
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+# p using_concat(num1, num2)
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
