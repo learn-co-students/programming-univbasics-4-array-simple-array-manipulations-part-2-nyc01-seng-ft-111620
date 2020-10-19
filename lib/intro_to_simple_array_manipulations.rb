66b2fdba115dfa187e35fa09eee56c2fa9a81a79
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-10-19 20:28:30 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(a,b)
+  a.concat(b)
+end
+
+def using_insert(a,b)
+  a.insert(4, b)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(a,b)
+  a.delete(b)
+end
+
+def using_delete_at(a,b)
+  a.delete_at(b)
+end
